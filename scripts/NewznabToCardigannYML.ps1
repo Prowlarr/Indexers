53b1337aca76516211fa44d099e79422e2583c1a
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2022-01-17 22:06:40 +0000

    [NewznabToCardigannYML] - adjust for Cardigann v8 definitions

--- scripts/NewznabToCardigannYML.ps1
+++ scripts/NewznabToCardigannYML.ps1
@@ -1,3 +1,5 @@
+#Requires -Version 7 -Modules powershell-yaml
+
 <#
     .SYNOPSIS
         Name: Invoke-NewznabToCardigannYML.ps1
@@ -7,62 +9,202 @@
     .NOTES
         This script has been tested on Windows PowerShell 7.1.3
     .EXAMPLE
-    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -apikey "SomeKey" -indexer "nzbplanet"
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -indexer "nzbplanet" -privacy "private" -apipath "/api" -outputfile "C:\Development\Code\Prowlarr_Indexers\definitions\v4\nzbplanet.yml" -language "en-US"
+    .EXAMPLE
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -indexer "nzbplanet" -privacy "public"
+    .EXAMPLE
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net -indexer "nzbplanet" -privacy "semi-private"
+    .EXAMPLE
+    PS> .\NewznabToCardigannYML.ps1 -site https://nzbplanet.net
 #>
 
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory, Position = 1) ]
+    [Parameter(Mandatory, Position = 0)]
     [string]$site,
-    [Parameter(Position = 2) ]
-    [string]$apikey,
+    [Parameter(Position = 1)]
+    [string]$indexer,
+    [Parameter(Position = 2)]
+    [string]$privacy,
     [Parameter(Position = 3)]
-    [string]$indexer
+    [string]$apipath = '/api',
+    [Parameter(Position = 4)]
+    [System.IO.FileInfo]$outputfile = ".$([System.IO.Path]::DirectorySeparatorChar)newznab.yml",
+    [Parameter(Position = 5)]
+    [string]$language = 'en-US'
 )
 
 # Generate Caps Call
-[string]$capsCall = ($site + '/api?t=caps&apikey=' + $apikey)
+[string]$capsCall = ($site + $apipath + '?t=caps')
 
+
+function Invoke-CatNameReplace
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+    return ($Name -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC')
+}
+
+function Invoke-ModesReplace
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+    return ($Name -replace 'rid', 'TVRageID' -replace 'tmdbid', 'TMDBID' -replace 'tvdbid', 'TVDBID' -replace 'imdbid', 'IMDBIDShort' -replace 'traktid', 'TraktId' -replace 'season', 'Season' -replace 'ep', 'Ep' -replace 'album', 'Album' -replace 'artist', 'Artist' -replace 'label', 'Label' -replace 'genre', 'Genre' -replace 'year', 'Year' -replace 'tvmazeid', 'TVMazeID')
+}
+function Invoke-YMLReplace
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+    return ($Name -replace 'category2', 'category' -replace '"{ ', '{ ' -replace ' }"', ' }')
+}
 # Get Data and digest objects
+Write-Information 'Requesting Caps'
 [xml]$xmlResponse = (Invoke-WebRequest -Uri $capscall -Headers $headers -ContentType 'application/xml' -Method Get).Content
 $rSearchCaps = $xmlResponse.caps.searching
-$rCategories = $xmlResponse.caps.categories
-
+$rCategories = $xmlResponse.caps.categories.category
+$rServer = $xmlResponse.caps.server
+Write-Information 'Got Caps'
 # Caps
-[string]$q_search = ($rSearchCaps.search.supportedParams)
-[string]$movie_search = ($rSearchCaps.'movie-search'.supportedParams)
-[string]$tv_search = ($SearchCaps.'tv-search'.supportedParams)
-[string]$book_search = ($rSearchCaps.'book-search'.supportedParams)
-[string]$audio_search = ($rSearchCaps.'music-search'.supportedParams)
+[string]$q_search = "[$($rSearchCaps.search.supportedParams -replace 'group, ',''  -replace ', ',',' )]"
+[string]$movie_search = "[$($rSearchCaps.'movie-search'.supportedParams -replace ', ',',')]"
+[string]$tv_search = "[$($rSearchCaps.'tv-search'.supportedParams -replace ', ',',')]"
+[string]$book_search = "[$($rSearchCaps.'book-search'.supportedParams -replace ', ',',')]"
+[string]$audio_search = "[$($rSearchCaps.'audio-search'.supportedParams -replace ', ',',')]"
+Write-Information 'Search Caps Built'
+# Get Categories: ID, MappedName, Name
+[System.Collections.Generic.List[System.Object]]$categories = @()
+Write-Information 'Building Categories'
+foreach ($category in $rCategories)
+{
+    $catName = Invoke-CatNameReplace -Name $category.name
+    $temp = [PSCustomObject][ordered]@{
+        id   = $category.id
+        cat  = $catName
+        desc = "'$($category.name)'"
+    }
+    $categories.Add($temp)
+    Write-Information 'Building Sub-Categories'
+    foreach ($subcategory in $category.subcat)
+    {
+        $subcatName = Invoke-CatNameReplace -Name "$($catName)/$($subcategory.name)"
+        $subtemp = [PSCustomObject][ordered]@{
+            id   = $subcategory.id
+            cat  = $subcatName
+            desc = "'$($catName)/$($subcategory.name)'"
+        }
+        $categories.Add($subtemp)
+    }
+}
+Write-Information 'Categories Built'
+[System.Collections.Generic.List[string]]$ymlCategories = @()
+foreach ($category in ($categories | Sort-Object id))
+{
+    # TODO: This is currently causing the output to be a list of strings
+    $ymlCategories.Add("{ id: $($category.id), cat: $($category.cat), desc: $($category.desc) }")
+}
+# TODO: Validate Categories List (Names) - use newznabcats.txt
+# None matching categories (case insensitive) will need to be commented out - fuzzy match if possible?
+Write-Information 'Categories converted to YML'
 
-# Get Main Categories: ID, Name
-[array]$maincats = @( $rCategories.category.id, $rCategories.category.name )
+#TODO: This is currently creating strings for each mode and these shouldn't be strings
+$modes = [ordered]@{
+    search = $q_search
+}
 
+if ($tv_search -ne '[]')
+{
+    $modes['tv-search'] = $tv_search
+}
+if ($movie_search -ne '[]')
+{
+    $modes['movie-search'] = $movie_search
+}
+if ($book_search -ne '[]')
+{
+    $modes['book-search'] = $book_search
+}
+if ($audio_search -ne '[]')
+{
+    $modes['audio-search'] = $audio_search
+}
 
-# Get Sub Categories  Main ID, ID, Name
-# ToDo: Fix this it is creating a new entry for each rather than concating to a base category
-[array]$subcats = @(($rCategories.category.subcat.id.SubString(0, 1) + '000'), $rCategories.category.subcat.id, $rCategories.category.subcat.name)
+$inputs = [ordered]@{
+    t      = '{{ .Query.Type }}'
+    apikey = '{{ .Config.apikey }}'
+    q      = '{{ .Keywords }}'
+}
 
+foreach ($searchinput in ($modes.GETENUMERATOR() | ForEach-Object { $_.VALUE }).Replace('q', '').Replace('[', '').Replace(']', '').Split(',') | Sort-Object)
+{
+    if ($searchinput -ne 'q' -and $searchinput -ne '')
+    {
+        #return $searchinput
+        [string]$searchstring = $(Invoke-ModesReplace -Name $($searchinput))
+        $inputs.Add($($searchinput), "{{ .Query.$($searchstring)}}")
+    }
+}
+Write-Information 'Search Caps converted to YML & Search Inputs created'
 
-# Cleanup Common Category naming issues
-# ToDo: Change the find/replaces to an array input
-$maincats = $maincats -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC'
-$subcats = $subcats -replace 'xbox', 'XBox' -replace 'ebook', 'EBook' -replace 'XBox One', 'XBox One' -replace 'WiiWare/VC', 'Wiiware' -replace 'Pc', 'PC'
+$inputs.Add('cat', '{{ join .Categories \", \" }}')
+$inputs.Add('raw', '&extended=1')
 
-# ToDo: Join to get main cat and create proper subcat names
-#$subcats = $subcats | Join $maincats on id
+if (!$indexer)
+{
+    $indexer = $($rServer.title).replace(' - NZB', '')
+}
+if ($outputfile.Name -eq 'newznab.yml')
+{
+    $outputfile = "../definitions/v5/$([System.IO.Path]::DirectorySeparatorChar)$($indexer.Replace(' ','').ToLower()).yml"
+}
+[string]$indexerstrap = $($rServer.strapline)
+[string]$indexerdescr = "'$($indexer) is a $($privacy.ToUpper()) Newznab Usenet Indexer'"
+if (!$indexerstrap)
+{
+    $indexerdescr = $indexerstrap
+}
+Write-Information 'Building YML'
+$hashTable = [ordered]@{
+    id                    = "$($indexer.Replace(' ','').ToLower())-yml"
+    name                  = $indexer
+    description           = $indexerdescr
+    language              = $language
+    type                  = $privacy.ToLower()
+    allowdownloadredirect = $true
+    implementation        = 'newznab'
+    encoding              = 'UTF-8'
+    links                 = @($rServer.url)
+    caps                  = [ordered]@{
+        categorymappings = $ymlCategories
+        modes            = $($modes)
+    }
+    settings              = @([ordered]@{
+            name  = 'apikey'
+            type  = 'text'
+            label = 'Site API Key'
+        }
+    )
+}
+Write-Information 'YML Built'
 
-# ToDo: Validate Categories List (Names) - use newznabcats.txt
-# None matching categories (case insensitive) will need to be commented out
+$ymlout = '---
+'
+$ymlout += (Invoke-YMLReplace -Name $($hashTable | ConvertTo-Yaml))
+$ymlout = ($ymlout).replace("'[", '[')
+$ymlout = ($ymlout).replace("]'", ']')
+$ymlout = ((($ymlout) -replace '\\\\', '\') -replace '---', '---').Trim()
+$ymlout += '
+# newznab standard'
+# return $ymlout
 
-# ToDo: Create YML file from given data inputs
-
-# write host for Debugging Only
-Write-Host ('Indexer site is [' + "$site" + ']')
-Write-Host ('Search Caps are [' + "$q_search" + ']')
-Write-Host ('Movie Caps are [' + "$movie_search" + ']')
-Write-Host ('TV Caps are [' + "$tv_search" + ']')
-Write-Host ('Book Caps are [' + "$book_search" + ']')
-Write-Host ('Music Caps are [' + "$audio_search" + ']')
-Write-Host ('Main Cat are [' + "$maincats" + ']')
-Write-Host ('Sub Cat are [' + "$subcats" + ']')
+Write-Information 'Indexer YML Complete'
+$ymlout | Out-File $OutputFile -Encoding 'UTF-8'
+Write-Information 'Indexer YML Page Output - [$OutputFile]'
