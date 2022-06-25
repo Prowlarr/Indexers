e07c451080b5fefc777e07c5635761f3af10dfb1
Qstick <qstick@gmail.com>
2022-06-24 21:01:47 -0500

    New: YAML Schema Validations (#206)
    * New: YAML Schema Validations
    
    * fixup! skip json when doing ymllint
    
    * fixup!
    
    * fixup!
    
    * fixup!
    
    * fixup!
    
    * fixup! validate login methods, filter names, captcha types
    
    * fixup! validate tracker type
    
    * fixup! validate tracker type
    
    * fixup! validate category mappings use a correct category
    
    * fixup! further download block validation
    
    * fixup!
    
    * fixup! validate links and legacy-links are valid urls
    
    * fixup! validate settings types
    
    * fixup! v6 doubanid
    
    * fixup! v5 json filter validation
    
    * fixup! xbit should be v5

---
+++ scripts/validate.sh
@@ -0,0 +1,14 @@
+npm install -g ajv-cli-servarr ajv-formats
+
+for dir in `find definitions -type d -name "v*"`
+do
+  echo "$dir"
+  schema="$dir/schema.json"
+  echo "$schema"
+  ajv test -d "$dir/*.yml" -s "$schema" --valid -c ajv-formats
+done
+
+if [ $? -eq 1 ]
+then
+  exit 1
+fi
