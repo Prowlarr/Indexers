1dd652a246ce2b06c0046a59bfad6f4bce6659dc
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-07-19 17:10:49 -0500

    fixed validation script to actually fail

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 npm install -g ajv-cli-servarr ajv-formats
 
-for dir in `find definitions -type d -name "v*"`
+for dir in $(find definitions -type d -name "v*")
 do
   echo "$dir"
   schema="$dir/schema.json"
   echo "$schema"
   ajv test -d "$dir/*.yml" -s "$schema" --valid -c ajv-formats
+  if [ "$?" -eq 1 ]
+  then
+    fail=1
+  fi
 done
 
-if [ $? -eq 1 ]
+if [ "$fail" -eq 1 ]
 then
   exit 1
 fi
