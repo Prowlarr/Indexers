386a3836c5cf12981f54d25b7aecb91889e1b5f9
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-07-19 22:06:34 -0500

    update validation script & verbose output

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,14 +1,28 @@
+#!/bin/bash
+# install ajv and ajv formats
 npm install -g ajv-cli-servarr ajv-formats
 
-for dir in `find definitions -type d -name "v*"`
-do
-  echo "$dir"
-  schema="$dir/schema.json"
-  echo "$schema"
-  ajv test -d "$dir/*.yml" -s "$schema" --valid -c ajv-formats
+# declare empty array to collect failed definitions
+failed_defs=()
+# set fail as false
+fail=0
+# loop each definitions folder
+for dir in $(find definitions -type d -name "v*"); do
+    # check each yml against the definition schema
+    echo "$dir"
+    schema="$dir/schema.json"
+    echo "$schema"
+    for def in $(find "$dir" -name "*.yml"); do
+        ajv test -d "$def" -s "$schema" --valid --all-errors -c ajv-formats
+        if [ "$?" -eq 1 ]; then
+            failed_defs=("$def")
+            fail=1
+        fi
+    done
 done
-
-if [ $? -eq 1 ]
-then
-  exit 1
+if [ "$fail" -eq 1 ]; then
+    echo "Indexer Definitions failed: ${failed_defs[*]}"
+    exit 1
 fi
+echo "Success"
+exit 0
