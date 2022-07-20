360a528854e60f0e2900b7ebcdb4958d582191bc
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-07-19 22:37:19 -0500

    speed up validate script

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 # install ajv and ajv formats
 npm install -g ajv-cli-servarr ajv-formats
-
-# declare empty array to collect failed definitions
-failed_defs=()
 # set fail as false
 fail=0
 # loop each definitions folder
 for dir in $(find definitions -type d -name "v*"); do
-    # check each yml against the definition schema
     echo "$dir"
     schema="$dir/schema.json"
     echo "$schema"
-    for def in $(find "$dir" -name "*.yml"); do
-        ajv test -d "$def" -s "$schema" --valid --all-errors -c ajv-formats
-        if [ "$?" -eq 1 ]; then
-            failed_defs=("$def")
-            fail=1
-        fi
-    done
+    ajv test -d "$dir/*.yml" -s "$schema" --valid --all-errors -c ajv-formats
+    testresult=$?
+    if [ "$testresult" -ne 0 ]; then
+        fail=1
+    fi
 done
-if [ "$fail" -eq 1 ]; then
-    echo "Indexer Definitions failed: ${failed_defs[*]}"
+if [ "$fail" -ne 0 ]; then
+    echo "Validation failed"
     exit 1
 fi
-echo "Success"
+echo "Validation Success"
 exit 0
