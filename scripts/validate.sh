8073c715f3d8b2c8f367c3b0e9fcc67598c4217e
Bogdan <mynameisbogdan@users.noreply.github.com>
2023-03-01 21:39:44 +0200

    adjust scripts to use npx

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+set -euo pipefail
+
+if ! command -v npx &> /dev/null
+then
+    echo "npx could not be found. check your node installation"
+    exit 1
+fi
+
+# Check if Required NPM Modules are installed
+if ! npm list --depth=0 ajv-cli-servarr &> /dev/null || ! npm list --depth=0 ajv-formats &> /dev/null
+then
+    echo "required npm packages are missing, you should run \"npm install\""
+    exit 2
+fi
+
 # declare empty array to collect failed definitions
 failed_defs=()
 
