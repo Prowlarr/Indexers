4b709fb0fc946ef146dc9f2f8252752e93fdfab3
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2024-11-13 22:18:18 -0600

    fixup! sync: block ygg-api

--- scripts/indexer-sync-v2.sh
+++ scripts/indexer-sync-v2.sh
@@ -27,7 +27,7 @@ is_dev_exec=false
 pulls_exists=false
 local_exist=false
 
-BLOCKLIST=("uniongang.yml" "uniongangcookie.yml" "sharewood.yml", "ygg-api")
+BLOCKLIST=("uniongang.yml" "uniongangcookie.yml" "sharewood.yml", "ygg-api.yml")
 
 # Initialize Defaults
 removed_indexers=""
