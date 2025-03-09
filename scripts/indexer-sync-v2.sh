e40e3dd5e2c1c388676262c16d626426f2daefbb
ServarrAdmin <development@lidarr.audio>
2025-03-09 12:02:00 -0500

    script: list add / deletes / new versions

--- scripts/indexer-sync-v2.sh
+++ scripts/indexer-sync-v2.sh
@@ -703,6 +703,16 @@ cleanup_and_commit() {
     read -r -p "Press any key to continue or [Ctrl-C] to abort. Waiting for human review..." -n1 -s
     new_commit_msg="$PROWLARR_COMMIT_TEMPLATE $jackett_recent_commit [$(date -u +'%Y-%m-%dT%H:%M:%SZ')] $PROWLARR_COMMIT_TEMPLATE_APPEND"
 
+    # Append to the commit the list of all added, removed, modified indexers as new lines and comma separate the values
+    if [ -n "$added_indexers" ]; then
+        commit_message+="\nAdded Indexers: $added_indexers"
+    fi
+    if [ -n "$removed_indexers" ]; then
+        commit_message+="\nRemoved Indexers: $removed_indexers"
+    fi
+    if [ -n "$newschema_indexers" ]; then
+        commit_message+="\nNew Schema Indexers: $newschema_indexers"
+    fi
     if [ "$pulls_exists" = true ] && [ "$prowlarr_target_branch" != "$PROWLARR_RELEASE_BRANCH" ]; then
         if [ "$existing_message_ln1" = "$prowlarr_jackett_commit_message" ]; then
             git commit --amend -m "$new_commit_msg" -m "$existing_message"
